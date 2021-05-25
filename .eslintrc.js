d4b752848e035493e035a8934bb029855906d874
theothergrantdavidson <grant.l.davidson@gmail.com>
2021-05-25 17:21:51 +1000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,6 @@
 const base = require('./.eslintrc.base.js');
-
-// Enable type-aware static analysis.
-// This will ask TypeScript to do a build of the project before ESLint.
 module.exports = {
     ...base,
-    // this is separated from the .eslint.base.js in order to speed up eslint in precommit hook
-    // using parserOptions will have TypeScript to do a build of a project before linting
-    // see:
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/TYPED_LINTING.md
     parserOptions: {
         project: __dirname + '/tsconfig.json',
         sourceType: 'module'
